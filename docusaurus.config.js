d2614eba59561715643c5450ac9f11e52c86ccac
123456 <123456>
2023-01-05 14:31:22 +0800

    11

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,6 +52,12 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        sitemap: {
+          changefreq: 'daily',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+        },
       }),
       
     ],
@@ -169,7 +175,7 @@ const config = {
   },
 
   plugins: [
-         
+
   ],
 };
 
